1a46e75706dda71300b951189bbca8f9d19bcf43
william <wlopezob@gmail.com>
2021-05-26 11:52:20 -0500

    sonarqube php

--- sonarqube/instancia.sh
+++ sonarqube/instancia.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws cloudformation deploy --region us-east-1 --stack-name ec2sonarqube  --parameter-overrides EnvironmentName="MiAmbiente" LlaveEc2="KeyEc2CloudFormationVirginia" --template-file ./instancia.yml
+aws cloudformation deploy --region us-east-1 --stack-name ec2sonarqube  --parameter-overrides EnvironmentName="Misonarqube" LlaveEc2="KeyEc2CloudFormationVirginia" --template-file ./instancia.yml
